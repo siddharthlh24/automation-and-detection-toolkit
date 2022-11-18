4c2698517d65717ad912c2a7f84437e68a2805f6
Siddharth L Hasanabadi <siddharth.lh@gmail.com>
2022-11-18 23:13:36 +0530

    * update base ip

--- source/wiz_utils.py
+++ source/wiz_utils.py
@@ -5,7 +5,7 @@ from error_codes import *
 async def wiz_discover():
     # Discover all bulbs in the network via broadcast datagram (UDP)
     # function takes the discovery object and returns a list of wizlight objects.
-    bulbs = await discovery.discover_lights(broadcast_space="192.168.0.255")
+    bulbs = await discovery.discover_lights(broadcast_space="192.168.1.255")
     # Print the IP address of the bulb on index 0
     #print(f"Bulb IP address: {bulbs[0].ip}")
 
